6cd1a77340cc1025e377f95b5d8944ebcae00a24
Pascal Reich <preich5404@sdsu.edu>
2025-09-30 16:35:08 -0400

    fix gh pages issure

--- scripts.js
+++ scripts.js
@@ -24,8 +24,8 @@ window.addEventListener('DOMContentLoaded', function() {
 
     function updateFragment(text) {
         // Don't spam the browser history & strip query strings.
-        window.location.replace(location.origin + '/#' + encodeURIComponent(text));
-        shareLinkField.value = location.origin + '/' + location.hash;
+        // window.location.replace(location.origin + '/#' + encodeURIComponent(text));
+        // shareLinkField.value = location.origin + '/' + location.hash;
     }
 
     function updateTitle(text) {
